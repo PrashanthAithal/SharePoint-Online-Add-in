06f6b0c5883adf40dd9ff490a80ae6250b4a1858
Prashanth Aithal <50479415+PrashanthAithal@users.noreply.github.com>
2019-08-11 21:26:02 +0530

    Add files via upload

---
+++ TND_SharePoint_Add-in/TND_SharePoint_Add-in/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="3558603b-3332-4874-b625-98266a8c325e" description="SharePoint Add-in Feature" featureId="3558603b-3332-4874-b625-98266a8c325e" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="TND_SharePoint_Add-in Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="740e451d-51f3-443e-a9da-50acb28a0a60" />
+    <projectItemReference itemId="ed6f0408-6ea7-4e7f-b53f-123f6eb04c27" />
+    <projectItemReference itemId="fa9693af-3135-4887-a1b5-3d1d076e58cb" />
+    <projectItemReference itemId="b57b1e51-b23a-4fed-9c2c-dcc48916dd7d" />
+  </projectItems>
+</feature>
---
+++ TND_SharePoint_Add-in/TND_SharePoint_Add-in/Scripts/App.js
@@ -0,0 +1,85 @@
+﻿'use strict';
+
+ExecuteOrDelayUntilScriptLoaded(initializePage, "sp.js");
+
+function initializePage() {
+
+    let hostweburl = decodeURIComponent(getQueryStringParameter('SPHostUrl'));
+    let appweburl = decodeURIComponent(getQueryStringParameter('SPAppWebUrl'));
+    var context = SP.ClientContext.get_current();
+    var user = context.get_web().get_currentUser();
+
+    // This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
+    $(document).ready(function () {
+        getUserName();
+    });
+
+    // This function prepares, loads, and then executes a SharePoint query to get the current users information
+    function getUserName() {
+        context.load(user);
+        context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
+    }
+
+    // This function is executed if the above call is successful
+    // It replaces the contents of the 'message' element with the user name
+    function onGetUserNameSuccess() {
+        $('#message').text('<b>Hello ' + user.get_title() + " Welcome to TND portal</b>");
+    }
+
+    // This function is executed if the above call fails
+    function onGetUserNameFail(sender, args) {
+        alert('Failed to get user name. Error:' + args.get_message());
+    }
+
+    // Initialize the RequestExecutor with the app web URL.
+    var scriptbase = hostweburl + "/_layouts/15/";
+    $.getScript(scriptbase + "SP.RequestExecutor.js", execCrossDomainRequest);
+
+    function execCrossDomainRequest() {
+        var executor = new SP.RequestExecutor(appweburl);
+    }
+    // Button click event.
+    $("#btnShowTraining").click(function () {
+
+        var executor = new SP.RequestExecutor(appweburl);
+        executor.executeAsync(
+            {
+                url: appweburl + "/_api/SP.AppContextSite(@target)/web/lists/getbytitle('Training')/Items?@target='"
+                    + hostweburl + "'",
+                method: "GET",
+                headers: { "Accept": "application/json; odata=verbose" },
+                success: successHandler,
+                error: errorHandler
+            }
+        );
+
+    });
+};
+
+// On query Success
+function successHandler(data) {
+    var trainingCollection = "";
+    var jsonObject = JSON.parse(data.body);
+  
+    var results = jsonObject.d.results;
+    for (var i = 0; i < results.length; i++) {
+        trainingCollection = trainingCollection + "<p><h3>" + results[i].Title + "</h3></p><hr>";
+    }
+
+    document.getElementById("resultsDiv").innerHTML = trainingCollection;
+}
+// Error handler.
+function errorHandler(error) {
+    $("#resultsDiv").append(error.statusText)
+}
+
+//Function to get a parameter value by a specific key
+function getQueryStringParameter(urlParameterKey) {
+    var params = document.URL.split('?')[1].split('&');
+    var strParams = '';
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split('=');
+        if (singleParam[0] == urlParameterKey)
+            return decodeURIComponent(singleParam[1]);
+    }
+}
